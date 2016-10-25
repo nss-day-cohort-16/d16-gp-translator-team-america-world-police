9c99248ded550dc679945f94c71bd457a0412078
Samuel Phillips <samphillips1879@gmail.com>
2016-10-25 10:12:50 -0500

    updated package.json to align with master

--- javascripts/german.js
+++ javascripts/german.js
@@ -1,11 +1,22 @@
 "use strict";
 
 var Translator = (function(oldTranslator){
+	var germanKey = {
+		"Happy": "Glücklich",
+		"Independence": "Unabhängigkeit",
+		"Day": "Tag" 
+	}
 
-	return {
-		translateToGerman: function(){
-			
-		}
-	};
+	oldTranslator.test = "is this IIFE connected";
+
+	oldTranslator.translateToGerman = function(inputPhrase){
+		inputPhrase.replace("Happy Independence Day", "Fröhlichen Unabhängigkeitstag");
+		inputPhrase.replace(/Happy|happy/g, "Glücklich");
+		inputPhrase.replace(/Independence|Independence/g, "unabhängigkeit");
+		inputPhrase.replace(/Day|day/g, "tag");
+
+			return inputPhrase;
+		};
+	return oldTranslator;
 
 })(Translator);
